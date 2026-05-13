50997307f2c89a047794ea73263781c1ad429983
slvedrd1 <slvedrd1@gmail.com>
2026-05-13 15:09:56 -0400

    validaciones

--- Primero.py
+++ Primero.py
@@ -16,39 +16,44 @@ while True:
 
     if Opcion == 1:
 
-        print("Pago de tarjeta")
-        MontoPagoCredito = int(input("Ingrese un monto para pagar la tarjeta de credito: $"))
+        try:
 
-        if MontoPagoCredito >= 0 and MontoPagoCredito <= DEUDA:
-            DEUDA -= MontoPagoCredito
-            print("Deuda: $",DEUDA)
-        else:
-            print("El monto a pagar excede la deuda.")
+            print("Pago de tarjeta")
+            MontoPagoCredito = int(input("Ingrese un monto para pagar la tarjeta de credito: $"))
 
-
-    elif Opcion == 2:
-
-        print("Simulacion de compra")
-        
-        
-        cantidad = int(input("Cuantas prendas desea comprar?: "))
-
-        for i in range(cantidad):
-
-            MontoCompra = int(input(f"\nIngrese el monto de la compra {i + 1}: $"))
-
-            if MontoCompra >= 0:
-                DEUDA += MontoCompra
-
+            if MontoPagoCredito >= 0 and MontoPagoCredito <= DEUDA:
+                DEUDA -= MontoPagoCredito
                 print("Deuda: $",DEUDA)
             else:
-                print("Error monto no valido, el numero no debe ser negativo")
+                print("El monto a pagar excede la deuda.")
+        
+        except ValueError:
+            print("Error, debe ingresar un valor numerico")
 
+    elif Opcion == 2:
+    
+        try: 
+            print("Simulacion de compra")
+            
+            cantidad = int(input("Cuantas prendas desea comprar?: "))
+
+            for i in range(cantidad):
+
+                MontoCompra = int(input(f"\nIngrese el monto de la compra {i + 1}: $"))
+
+                if MontoCompra >= 0:
+                    
+                    DEUDA += MontoCompra
+                    print("Deuda: $",DEUDA)
+                else:
+                    print("Error monto no valido, el numero no debe ser negativo")
+        
+        except ValueError:
+            print("Error, debe ingresar un valor numerico")
 
     elif Opcion == 3:
         print("Saliendo..")
         break
-
     else:
         print("Opcion no valida, intente otra vez.")
         
